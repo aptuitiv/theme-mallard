9e24cf273c8d4efa919a5cb16d696a093742d43a
Eric Tompkins <eric@aptuitiv.com>
2026-09-22 11:42:40 -0400

    Removed unused getCookie method
    It was the same code as setCookie with the wrong name

--- src/js/lib/cookie.js
+++ src/js/lib/cookie.js
@@ -20,25 +20,6 @@ export const getCookieValue = (cname) => {
  * @param {string} cvalue Cookie value
  * @param {number} exdays Number of days to set cookie for
  */
-export const getCookie = (cname, cvalue, exdays) => {
-    const d = new Date();
-    const existing = getCookieValue(cname);
-    let cookievalue = cvalue;
-    if (existing.length > 0) {
-        cookievalue = `${existing}-${cvalue}`;
-    }
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    const expires = `expires=${d.toUTCString()}`;
-    document.cookie = `${cname}=${cookievalue}; ${expires};path=/`;
-};
-
-/**
- * Set a cookie
- *
- * @param {string} cname Cookie name
- * @param {string} cvalue Cookie value
- * @param {number} exdays Number of days to set cookie for
- */
 export const setCookie = (cname, cvalue, exdays) => {
     const d = new Date();
     const existing = getCookieValue(cname);
@@ -46,7 +27,7 @@ export const setCookie = (cname, cvalue, exdays) => {
     if (existing.length > 0) {
         cookievalue = `${existing}-${cvalue}`;
     }
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
     const expires = `expires=${d.toUTCString()}`;
     document.cookie = `${cname}=${cookievalue}; ${expires};path=/`;
 };
